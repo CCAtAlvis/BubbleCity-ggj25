19d28e38162422c1101c9bbfd9c3c5fa41f6d534
Barath Nadar <barathnadar@gofynd.com>
2025-01-26 03:36:22 +0530

    Drag and Drop

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ public class TileController : MonoBehaviour
     public bool IsWithinDiameter => isWithinDiameter;
     public void SetWithinDiameter(bool active) => isWithinDiameter = active;
 
-    private GameObject structureType;
+    public GameObject structureType;
     public GameObject highlighter;
 
     private UnityEngine.Color[] HighlightColor = {new Color( 0f , 1f , 0f , 0.3f ),new Color( 1f , 0 , 0 , 0.3f )};
@@ -25,11 +26,15 @@ public class TileController : MonoBehaviour
 
     void OnMouseOver()
     {
-        highlighter.SetActive(true);
-        if(structureType.IsUnityNull()){
-            highlighter.GetComponent<SpriteRenderer>().color = HighlightColor[0];
-        }else{
-            highlighter.GetComponent<SpriteRenderer>().color = HighlightColor[1];
+        if(GameManager.GetInstance().isHoverActive){
+            highlighter.SetActive(true);
+            GameManager.GetInstance().isHoveredGridEmpty = structureType.IsUnityNull();
+            GameManager.GetInstance().hoveredGrid = this;
+            if(structureType.IsUnityNull()){
+                highlighter.GetComponent<SpriteRenderer>().color = HighlightColor[0];
+            }else{
+                highlighter.GetComponent<SpriteRenderer>().color = HighlightColor[1];
+            }
         }
     } 
 
