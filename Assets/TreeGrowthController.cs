a42cf6b4cdb5d8d8fcad461449cc492f72e04e2b
Arvindraj Thangaraj <arvindrajthangaraj@gmail.com>
2025-01-25 14:54:09 +0530

    Temporarily use House Sprite for Tree; Sprite usage for Growth Controller
    Signed-off-by: Arvindraj Thangaraj <arvindrajthangaraj@gmail.com>

--- Assets/TreeGrowthController.cs
+++ Assets/TreeGrowthController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TreeGrowthController : MonoBehaviour
 {
@@ -6,30 +7,40 @@ public class TreeGrowthController : MonoBehaviour
     public int TREE_SECOND_THRESHOLD_DURATION = 10;
     float startTime;
     public int Level;
+    public Sprite TreeSpriteLevel0;
+    public Sprite TreeSpriteLevel1;
+    public Sprite TreeSpriteLevel2;
+    private List<Sprite> TreeSprites;
+    private SpriteRenderer spriteRenderer;
+    private void UpdateSprite() {        
+        spriteRenderer.sprite = TreeSprites[Level];
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startTime = Time.time;
+        TreeSprites = new() { TreeSpriteLevel0, TreeSpriteLevel1, TreeSpriteLevel2 };
         Level = 0;
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        UpdateSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
-        var currentTime = Time.time;
-        var difference = currentTime - startTime;
+        float currentTime = Time.time;
+        float difference = currentTime - startTime;
+        
         if (difference > TREE_FIRST_THRESHOLD_DURATION)
         {
             Level = 1;
-            spriteRenderer.color = new Color(0, 0, 1);
-
+            UpdateSprite();
         }
         if (difference > TREE_SECOND_THRESHOLD_DURATION)
         {
             Level = 2;
-            spriteRenderer.color = new Color(0, 1, 0);
+            UpdateSprite();
         }
     }
 }
