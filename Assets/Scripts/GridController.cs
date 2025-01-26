73daa2b91974e52686bffdb15505224bc56f67e2
Nathan <savardnm@gmail.com>
2025-01-26 11:52:41 +0530

    update grid calculation to use an integer-based coordinates with float basis vectors

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -7,13 +7,20 @@ public class GridController : MonoBehaviour
     private static GridController instance;
     public static GridController GetInstance() => instance;
 
-    public int initialGridDiameter = 3;
+    public int initialGridDiameter = 5;
     public int bufferGridDiameter = 1;
     public float diameterVolumeMultiplier = 0.65f;
     public GameObject tilePrefab;
 
     private int maxGridDiameter = 0;
     private Dictionary<Vector2, TileController> coordinateToTile = new Dictionary<Vector2, TileController>();
+
+    private static float gridXOffset = 0.4075f;
+
+    private static float gridYOffset = 0.21f;
+
+    private Vector2 gridXBasis = new Vector2(gridXOffset, -gridYOffset);
+    private Vector2 gridYBasis = new Vector2(gridXOffset, gridYOffset);
 
     void Awake()
     {
@@ -57,38 +64,38 @@ public class GridController : MonoBehaviour
         {
             for (int j = -totalSize; j <= totalSize; j++)
             {
-                var xOffset = 0f;
-                if (Mathf.Abs(j) % 2 == 1)
-                {
-                    xOffset = 0.9f;
-                }
+                // var xOffset = 0f;
+                // if (Mathf.Abs(j) % 2 == 1)
+                // {
+                //     xOffset = 0.9f;
+                // }
 
-                float jFloat = j;
-                float iFloat = i;
-                if (xOffset > 0)
-                {
-                    jFloat = j - 1 + 0.5f;
-                    iFloat = i - 1 + 0.5f;
-                }
-                Vector2 coord = new Vector2(iFloat, jFloat);
+                // float jFloat = j;
+                // float iFloat = i;
+                // if (xOffset > 0)
+                // {
+                //     jFloat = j - 1 + 0.5f;
+                //     iFloat = i - 1 + 0.5f;
+                // }
+                Vector2 coord = i * gridXBasis + j * gridYBasis;
 
 
                 // Skip if we already have a tile at this coordinate
                 if (coordinateToTile.ContainsKey(coord))
                 {
                     // Update tile active state based on whether it's within bounds
-                    var isWithinBounds = IsWithinGridBounds(iFloat, jFloat, (diameter - 1) / 2);
+                    var isWithinBounds = IsWithinGridBounds(i, j, (diameter - 1) / 2);
                     coordinateToTile[coord].SetWithinDiameter(isWithinBounds);
                 }
                 else
                 {
                     var tile = Instantiate(tilePrefab);
-                    tile.transform.position = new Vector3(xOffset + i * 1.8f, j * 0.45f, j * -1);
+                    tile.transform.position = new Vector3(coord.x, coord.y, j * -1);
                     // tile.sortingOrder = j * -1;
 
                     // Add TileController component and configure it
                     var tileController = tile.gameObject.GetComponentInChildren<TileController>();
-                    bool isWithinMainGrid = IsWithinGridBounds(iFloat, jFloat, (diameter - 1) / 2);
+                    bool isWithinMainGrid = IsWithinGridBounds(i, j, (diameter - 1) / 2);
                     tileController.SetWithinDiameter(isWithinMainGrid);
 
                     // Store in our coordinate mapping
@@ -98,9 +105,14 @@ public class GridController : MonoBehaviour
         }
     }
 
-    private bool IsWithinGridBounds(float x, float y, int radius)
+    // private bool IsWithinGridBounds(float x, float y, int radius)
+    // {
+    //     return Mathf.Abs(x) <= radius && Mathf.Abs(y) <= radius;
+    // }
+
+    private bool IsWithinGridBounds(int i, int j, int radius)
     {
-        return Mathf.Abs(x) <= radius && Mathf.Abs(y) <= radius;
+        return Mathf.Sqrt(Mathf.Pow(i,2) + Mathf.Pow(j,2)) <= radius;
     }
 
     public TileController GetTileAt(Vector2Int coordinate)
