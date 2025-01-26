bfd02cc017973ec9df4f56965c3b313b5d95aca1
Barath Nadar <barathnadar@gofynd.com>
2025-01-26 05:37:05 +0530

    Fixed placement

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -50,6 +50,7 @@ public class TileController : MonoBehaviour
     void OnMouseExit()
     {
         highlighter.SetActive(false);
+        GameManager.GetInstance().isHoveredGridEmpty = false;
     }
 
 }
